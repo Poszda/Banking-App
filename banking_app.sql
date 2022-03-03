7c89412a0526987690a0da5fa5d9771f5ee6e5d6
Poszda <posdaresculg@gmail.com>
2022-03-03 16:24:54 +0200

    second commit

--- banking_app.sql
+++ banking_app.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 01, 2022 at 08:40 PM
+-- Generation Time: Mar 03, 2022 at 03:23 PM
 -- Server version: 10.4.22-MariaDB
 -- PHP Version: 7.3.33
 
@@ -42,11 +42,12 @@ CREATE TABLE `cards` (
 --
 
 INSERT INTO `cards` (`id`, `id_client`, `number`, `cvv`, `full_name`, `expiration_date`, `blocked`) VALUES
-(1, 1, '4679190007880987', 656, 'POSDARESCU MIHAI', '2027-02-12', 0),
+(1, 1, '4679190007880987', 656, 'POSDARESCU MIHAI', '2027-02-12', 1),
 (2, 2, '9934556711129090', 555, 'DORO ANA', '2025-01-22', 1),
 (3, 3, '1236632100001112', 154, 'POPE ALEXANDRU', '2027-01-07', 0),
 (6, 6, '2231190025899981', 433, 'BONO GINA', '2027-02-10', 0),
-(9, 11, '2472190307810228', 233, 'POM DORIAN', '2026-02-11', 0);
+(10, 12, '2121191099989989', 202, 'DUMBRAVA IRINA', '2026-04-15', 0),
+(11, 14, '1111192211180217', 203, 'RONY JOHN', '2026-03-04', 0);
 
 -- --------------------------------------------------------
 
@@ -55,7 +56,7 @@ INSERT INTO `cards` (`id`, `id_client`, `number`, `cvv`, `full_name`, `expiratio
 --
 
 CREATE TABLE `clients` (
-  `id` int(11) NOT NULL,
+  `id` int(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `first_name` varchar(100) NOT NULL,
   `security_code` varchar(100) NOT NULL,
@@ -68,11 +69,12 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`id`, `last_name`, `first_name`, `security_code`, `IBAN`, `account_balance`) VALUES
-(1, 'Posdarescu', 'Mihai', 'zhhh12', 'RO19RNCB555599990000', 3199),
-(2, 'Doro', 'Ana', 'ddaa11', 'RO19RNCB111122223333', 1539),
-(3, 'Pope', 'Alexandru', '123fff', 'GE14RNCB777766665555', 3520),
+(1, 'Posdarescu', 'Mihai', 'zhhh12', 'RO19RNCB555599990000', 3089),
+(2, 'Doro', 'Ana', 'ddaa11', 'RO19RNCB111122223333', 2088),
+(3, 'Pope', 'Alexandru', '123fff', 'GE14RNCB777766665555', 3640),
 (6, 'Bono', 'Gina', '11er', 'HU20RNCB515194492020', 99),
-(11, 'Pom', 'Dorian', '22rr1u', 'RO07RNCB443592992001', 155);
+(12, 'Dumbrava', 'Irina', 'erty112', 'RO19RNCB333311112022', 200),
+(14, 'Rony', 'John', '123aaa123', 'RO19RNCB225522992200', 100);
 
 -- --------------------------------------------------------
 
@@ -115,13 +117,22 @@ INSERT INTO `transactions` (`id`, `id_giver`, `id_receiver`, `amount`, `data`) V
 (112, NULL, 8, 80, '2022-02-03 20:05:34.593038'),
 (113, NULL, 1, 100, '2022-02-03 20:24:16.040149'),
 (114, NULL, 1, 100, '2022-02-03 20:24:17.667030'),
-(115, NULL, 11, 20, '2022-02-03 20:30:01.177431'),
 (116, NULL, 1, 50, '2022-02-08 20:17:45.993993'),
 (117, 1, 2, 317, '2022-02-08 20:18:51.884430'),
 (118, NULL, 1, 99, '2022-02-16 11:44:24.373816'),
 (119, NULL, 1, 100, '2022-03-01 19:10:47.468696'),
 (120, NULL, 1, 22, '2022-03-01 19:14:39.854920'),
-(121, 1, 2, 22, '2022-03-01 19:15:24.271954');
+(121, 1, 2, 22, '2022-03-01 19:15:24.271954'),
+(122, NULL, 1, 200, '2022-03-03 13:56:09.649807'),
+(123, NULL, 1, 100, '2022-03-03 13:56:54.459768'),
+(124, 1, 2, 499, '2022-03-03 14:01:00.041331'),
+(125, NULL, 1, 50, '2022-03-03 14:01:07.147894'),
+(126, 1, 2, 50, '2022-03-03 14:09:20.972095'),
+(127, NULL, 1, 100, '2022-03-03 14:09:28.814528'),
+(128, 1, 3, 100, '2022-03-03 14:13:52.131406'),
+(129, NULL, 1, 20, '2022-03-03 14:13:58.908884'),
+(130, 1, 3, 20, '2022-03-03 14:15:05.858942'),
+(131, NULL, 1, 89, '2022-03-03 14:15:11.468743');
 
 -- --------------------------------------------------------
 
@@ -131,7 +142,7 @@ INSERT INTO `transactions` (`id`, `id_giver`, `id_receiver`, `amount`, `data`) V
 
 CREATE TABLE `users` (
   `id` int(100) NOT NULL,
-  `id_client` int(11) NOT NULL,
+  `id_client` int(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -144,8 +155,7 @@ INSERT INTO `users` (`id`, `id_client`, `email`, `password`) VALUES
 (1, 1, 'posdaresculg@gmail.com', '123456'),
 (2, 2, 'doroana@gmail.com', '123456'),
 (3, 3, 'popealexandru@gmail.com', '123456'),
-(4, 6, 'bonogina@yahoo.com', '123456'),
-(7, 11, 'pom.dorian@e-uvt.ro', '123456');
+(4, 6, 'bonogina@yahoo.com', '123456');
 
 --
 -- Indexes for dumped tables
@@ -155,7 +165,8 @@ INSERT INTO `users` (`id`, `id_client`, `email`, `password`) VALUES
 -- Indexes for table `cards`
 --
 ALTER TABLE `cards`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `constrangere1` (`id_client`);
 
 --
 -- Indexes for table `clients`
@@ -173,7 +184,8 @@ ALTER TABLE `transactions`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `constrangere2` (`id_client`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -183,25 +195,41 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `cards`
 --
 ALTER TABLE `cards`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `transactions`
 --
 ALTER TABLE `transactions`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=122;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=132;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cards`
+--
+ALTER TABLE `cards`
+  ADD CONSTRAINT `constrangere1` FOREIGN KEY (`id_client`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `constrangere2` FOREIGN KEY (`id_client`) REFERENCES `clients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
